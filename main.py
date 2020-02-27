c56fd2666430aefb237de11ab7f2bd6e79bf0ec6
Athos Couto <athos.couto@vtex.com.br>
2020-02-27 14:53:51 -0300

    Fixed 1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from get_name import name
 # Did 4
 # Did 5
 # Did 3
-# Screwed up code 1
+# Fixed 1
 
 if __name__ == "__main__":
   print("Hello, git!" + (" My name is {}!".format(name) if name else ""))
